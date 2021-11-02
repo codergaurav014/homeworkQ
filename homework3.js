f548cc5eca0204bd327926ce244d157d0a921457
Gaurav Singh <87228988+codergaurav014@users.noreply.github.com>
2021-11-02 18:32:47 +0530

    Add files via upload

---
+++ homework3.js
@@ -0,0 +1,5 @@
+motercycle = ["tvs","bmw","honda","tata","tesla"];
+for (let i = 0; i < 5; i=i + 1)
+{
+    console.log(motercycle[i]);
+}
---
+++ homework4.js
@@ -0,0 +1,7 @@
+listofdays = ["day1","day2","day3","day4","day5","day6",];
+dancetime = [1,2,1,2,1,2]
+for (let i = 0; i <= 5; i=i + 1)
+{
+    console.log(listofdays[i]);
+    console.log(dancetime[i]);
+}
