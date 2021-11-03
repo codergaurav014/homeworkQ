aa0e572023e780fa4fa1563c65a48643405276c7
Gaurav Singh <87228988+codergaurav014@users.noreply.github.com>
2021-11-03 15:20:51 +0530

    Add files via upload

---
+++ homeworkq?.js
@@ -0,0 +1,16 @@
+james = 12
+smith = 49
+andy = 23
+
+if(james>andy){
+    console.log(james-6)
+    console.log(smith+2)
+    console.log(andy+4)
+    
+}else{
+    console.log(james+10)
+    console.log(smith-17)
+    console.log(andy+7)
+}
+
+
